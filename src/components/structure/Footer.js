2dfd6876310bd2766714121a51b697e61b3ba6f0
mnq11 <mnq_77@yahoo.com>
2023-03-16 16:34:30 +0100

    fixing the footer

--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -2,19 +2,29 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer has-bg-image4">
-      <div className="content has-text-centered has-text-white">
-        <p>
-          Built at <i className="fas fa-moon"></i> by{" "}
-          <a href="https://github.com/mnq11" className="has-text-white">
-            <strong>Mohammed Nabil</strong>
-          </a>
-        </p>
-        <p>
+      <footer className="footer has-bg-image4">
+        <div className="content has-text-centered has-text-white">
 
-        </p>
-      </div>
-    </footer>
+          <p>
+            Let's connect on
+            <a href="https://www.linkedin.com/in/mohammed-nabil-48b539107/" className="has-text-white">
+              <i>  </i>
+              <i className=" fab fa-linkedin"> </i> LinkedIn
+            </a>
+            <p>  </p>
+            <a href="mailto:mnq_77@yahoo.com" className="has-text-white">
+              <i className="far fa-envelope"></i>    Email me and discuss the next big thing!
+            </a>
+          </p>
+          <p>
+            &copy; 2023  <a href="https://github.com/mnq11" className="has-text-white">
+            <strong> Mohammed Nabil</strong>
+          </a>. All rights reserved.
+          </p>
+        </div>
+      </footer>
+
+
   );
 }
 
